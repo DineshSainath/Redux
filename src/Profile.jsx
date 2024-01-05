96a7a84e31f49698ab3659806d913bd7a7f8dacc
dineshsainath <sainath.dinesh@gmail.com>
2024-01-05 14:04:35 +0530

    Login and Profile components

---
+++ src/Profile.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Profile() {
+  return (
+    <div>
+      <h1>Profile Info</h1>
+      <h2>Name: </h2>
+      <h2>Age: </h2>
+      <h2>Email: </h2>
+    </div>
+  )
+}
+
+export default Profile
