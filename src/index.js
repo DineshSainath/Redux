9eb6413e8f588f8826dd4e70859f7fad7cb5d986
dineshsainath <sainath.dinesh@gmail.com>
2024-01-05 14:22:06 +0530

    store and reducers

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { configureStore } from '@reduxjs/toolkit'
+import { Provider } from 'react-redux'
 
+// store - where all the state lies for the application
+//reducers - to handle state operations or changes
+const store = configureStore({
+  reducer: {},
+})
+
+//Provider wraps the content where redux is used and we specify the store(states)
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
